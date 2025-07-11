869813eebd7cf79745d847b82d7ccd20aae80f1c
Reece <whitaker.reece@gmail.com>
2025-07-11 17:44:16 -0400

    setup

---
+++ README.md
@@ -0,0 +1,7 @@
+WORK IN PROGRESS
+# Meal Assistant
+A tool to help choose meals. Remedies decision paralysis and fatigue.
+
+## Goals
+1. Learning, second c# project
+2. A tool that is actually useful to me
--- obj/Debug/net8.0/Meal-Assistant.AssemblyInfo.cs
+++ obj/Debug/net8.0/Meal-Assistant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Meal-Assistant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285b94e820bed54de791f441dc9c7463e786f938")]
 [assembly: System.Reflection.AssemblyProductAttribute("Meal-Assistant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Meal-Assistant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
